1ca13fbbf250e687173f59315274257881a63fad
Eileen Mack <eileenmack@Eileens-MacBook-Air-3.local>
2016-03-08 12:11:49 -0500

    added box to data model

---
+++ MyStore/BoxViewController.h
@@ -0,0 +1,13 @@
+//
+//  BoxViewController.h
+//  MyStore
+//
+//  Created by Eileen Mack on 3/8/16.
+//  Copyright (c) 2016 Eileen Mack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BoxViewController : UITableViewController
+
+@end
